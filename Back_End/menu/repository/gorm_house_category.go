6440c4b2ed70683a5153dbc22fab88bcaa2face4
yegeta fekadu <ygyegeta@gmail.com>
2020-01-11 09:30:07 +0300

    gorm house status and update gorm house category

--- Back_End/menu/repository/gorm_house_category.go
+++ Back_End/menu/repository/gorm_house_category.go
@@ -14,61 +14,61 @@ func NewHCategoryGormRepo(db *gorm.DB) menu.CategoryRepository {
 	return &HCategoryGormRepo{conn: db}
 }
 
-func (cRepo *HCategoryGormRepo) Categories() ([]entity.House_category, []error) {
+func (HcRepo *HCategoryGormRepo) Categories() ([]entity.House_category, []error) {
 	ctgs := []entity.House_category{}
-	errs := cRepo.conn.Find(&ctgs).GetErrors()
+	errs := HcRepo.conn.Find(&ctgs).GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
 	return ctgs, errs
 }
-func (cRepo *HCategoryGormRepo) Category(id uint) (*entity.House_category, []error) {
+func (HcRepo *HCategoryGormRepo) Category(id uint) (*entity.House_category, []error) {
 	ctg := entity.House_category{}
-	errs := cRepo.conn.First(&ctg, id).GetErrors()
+	errs := HcRepo.conn.First(&ctg, id).GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
 	return &ctg, errs
 }
 
-func (cRepo *HCategoryGormRepo) UpdateCategory(category *entity.House_category) (*entity.House_category, []error) {
+func (HcRepo *HCategoryGormRepo) UpdateCategory(category *entity.House_category) (*entity.House_category, []error) {
 	cat := category
-	errs := cRepo.conn.Save(cat).GetErrors()
+	errs := HcRepo.conn.Save(cat).GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
 	return cat, errs
 }
 
-func (cRepo *HCategoryGormRepo) DeleteCategory(id uint) (*entity.House_category, []error) {
-	cat, errs := cRepo.Category(id)
+func (HcRepo *HCategoryGormRepo) DeleteCategory(id uint) (*entity.House_category, []error) {
+	cat, errs := HcRepo.Category(id)
 	if len(errs) > 0 {
 		return nil, errs
 	}
-	errs = cRepo.conn.Delete(cat, cat.ID).GetErrors()
+	errs = HcRepo.conn.Delete(cat, cat.ID).GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
 	return cat, errs
 }
-func (cRepo *HCategoryGormRepo) StoreCategory(category *entity.House_category) (*entity.House_category, []error) {
+func (HcRepo *HCategoryGormRepo) StoreCategory(category *entity.House_category) (*entity.House_category, []error) {
 	cat := category
-	errs := cRepo.conn.Create(cat).GetErrors()
+	errs := HcRepo.conn.Create(cat).GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
 	return cat, errs
 }
 
-func (cRepo *HCategoryGormRepo) HouseTypesInCategory(category *entity.House_category) ([]entity.HouseType, []error) {
+func (HcRepo *HCategoryGormRepo) HouseTypesInCategory(category *entity.House_category) ([]entity.HouseType, []error) {
 	HouseTypes := []entity.HouseType{}
-	cat, errs := cRepo.Category(category.ID)
+	cat, errs := HcRepo.Category(category.ID)
 
 	if len(errs) > 0 {
 		return nil, errs
 	}
 
-	errs = cRepo.conn.Model(cat).Related(&HouseTypes, "HouseTypes").GetErrors()
+	errs = HcRepo.conn.Model(cat).Related(&HouseTypes, "HouseTypes").GetErrors()
 	if len(errs) > 0 {
 		return nil, errs
 	}
