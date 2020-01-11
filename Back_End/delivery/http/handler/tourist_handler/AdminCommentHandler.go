9219cdf3d2c89bcf247460b1d306e04de250ce38
yegeta fekadu <ygyegeta@gmail.com>
2020-01-11 12:54:57 +0300

    tourist handlers

---
+++ Back_End/delivery/http/handler/tourist_handler/AdminCommentHandler.go
@@ -0,0 +1,182 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gplus1/HouseRentalSystem/Back_End/FeedBack"
+	"github.com/gplus1/HouseRentalSystem/Back_End/entity"
+	"github.com/gplus1/julienschmidt/httprouter"
+)
+
+// landLordFeedBackHandler handles FeedBack related http requests
+type landLordFeedBackHandler struct {
+	FeedBackService FeedBack.FeedBackService
+}
+
+// NewlandLordFeedBackHandler returns new landLordFeedBackHandler object
+func NewlandLordFeedBackHandler(cmntService FeedBack.FeedBackService) *landLordFeedBackHandler {
+	return &landLordFeedBackHandler{FeedBackService: cmntService}
+}
+
+// GetFeedBacks handles GET /v1/landLord/FeedBacks request
+func (ach *landLordFeedBackHandler) GetFeedBacks(w http.ResponseWriter,
+	r *http.Request, _ httprouter.Params) {
+
+	FeedBacks, errs := ach.FeedBackService.FeedBacks()
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(FeedBacks, "", "\t\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+// GetSingleFeedBack handles GET /v1/landLord/FeedBacks/:id request
+func (ach *landLordFeedBackHandler) GetSingleFeedBack(w http.ResponseWriter,
+	r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	FeedBack, errs := ach.FeedBackService.FeedBack(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(FeedBack, "", "\t\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+// PostFeedBack handles POST /v1/landLord/FeedBacks request
+func (ach *landLordFeedBackHandler) PostFeedBack(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	l := r.ContentLength
+	body := make([]byte, l)
+	r.Body.Read(body)
+	FeedBack := &entity.FeedBack{}
+
+	err := json.Unmarshal(body, FeedBack)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	FeedBack, errs := ach.FeedBackService.StoreFeedBack(FeedBack)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	p := fmt.Sprintf("/v1/landLord/FeedBacks/%d", FeedBack.ID)
+	w.Header().Set("Location", p)
+	w.WriteHeader(http.StatusCreated)
+	return
+}
+
+// PutFeedBack handles PUT /v1/landLord/FeedBacks/:id request
+func (ach *landLordFeedBackHandler) PutFeedBack(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	FeedBack, errs := ach.FeedBackService.FeedBack(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	l := r.ContentLength
+
+	body := make([]byte, l)
+
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &FeedBack)
+
+	FeedBack, errs = ach.FeedBackService.UpdateFeedBack(FeedBack)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(FeedBack, "", "\t\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+// DeleteFeedBack handles DELETE /v1/landLord/FeedBacks/:id request
+func (ach *landLordFeedBackHandler) DeleteFeedBack(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	_, errs := ach.FeedBackService.DeleteFeedBack(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
---
+++ Back_End/delivery/http/handler/tourist_handler/AdminRoleHandler.go
@@ -0,0 +1,69 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gplus1/HouseRentalSystem/Back_End/user"
+	"github.com/gplus1/julienschmidt/httprouter"
+)
+
+// landLordRoleHandler is used to implement role related http requests
+type landLordRoleHandler struct {
+	roleService user.RoleService
+}
+
+// NewlandLordRoleHandler initializes and returns new landLordRoleHandler object
+func NewlandLordRoleHandler(roleSrv user.RoleService) *landLordRoleHandler {
+	return &landLordRoleHandler{roleService: roleSrv}
+}
+
+// GetRoles handles GET /v1/landLord/roles requests
+func (arh *landLordRoleHandler) GetRoles(w http.ResponseWriter,
+	r *http.Request, _ httprouter.Params) {
+
+	roles, errs := arh.roleService.Roles()
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(roles, "", "\t\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+// GetSingleRole handles GET /v1/landLord/roles/:id requests
+func (arh *landLordRoleHandler) GetSingleRole(w http.ResponseWriter,
+	r *http.Request, ps httprouter.Params) {
+
+}
+
+// PutRole handles PUT /v1/landLord/roles/:id requests
+func (arh *landLordRoleHandler) PutRole(w http.ResponseWriter,
+	r *http.Request, ps httprouter.Params) {
+
+}
+
+// PostRole handles POST /v1/landLord/roles requests
+func (arh *landLordRoleHandler) PostRole(w http.ResponseWriter,
+	r *http.Request, _ httprouter.Params) {
+
+}
+
+// DeleteRole handles DELETE /v1/landLord/roles/:id requests
+func (arh *landLordRoleHandler) DeleteRole(w http.ResponseWriter,
+	r *http.Request, ps httprouter.Params) {
+
+}
